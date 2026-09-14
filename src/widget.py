a321f077474ff2027b3b7d35fd7709c94c7e5107
Диана Парамонова <dparamonova638@gmail.com>
2026-09-14 15:48:13 +0300

    fixing func mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,13 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
 def mask_account_card(card: str) -> str:
     """Функция, которая возвращает строку с замаскированным номером"""
+    if card == "" or card == " ": return " "
     if card[:4] != "Счет":
-        number_card = card[-16:]
-        answer = number_card[:4] + " "
-        answer += number_card[4:6] + "** "
-        answer += "**** " + number_card[-4:]
-        return  card[:-16] + answer
+        return card[:-16] + get_mask_card_number(card[-16:])
     else:
-        return card[:4] + " **" + card[-4:]
+        return card[:5] + get_mask_account(card)
 
 def get_date(data: str) -> str:
     return data[8:10] +"." + data[5:7] + "." + data[:4]
-
-print(get_date("2024-03-11T02:26:18.671407"))
