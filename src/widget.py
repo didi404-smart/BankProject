fc42baafd33d01bfe440933a5687a75e08472513
Диана Парамонова <dparamonova638@gmail.com>
2026-09-14 15:54:08 +0300

    edit pep 8/homework2

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,15 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card: str) -> str:
     """Функция, которая возвращает строку с замаскированным номером"""
-    if card == "" or card == " ": return " "
+    if card == "" or card == " ":
+        return " "
     if card[:4] != "Счет":
         return card[:-16] + get_mask_card_number(card[-16:])
     else:
         return card[:5] + get_mask_account(card)
 
+
 def get_date(data: str) -> str:
-    return data[8:10] +"." + data[5:7] + "." + data[:4]
+    return data[8:10] + "." + data[5:7] + "." + data[:4]
