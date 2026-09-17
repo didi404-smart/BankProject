930269f3656d3156dcd0859489348db77ed0367b
Диана Парамонова <dparamonova638@gmail.com>
2026-09-17 11:14:01 +0300

    fixed названия переменных

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,17 @@
-def filter_by_state(list_dictionary: list[dict], status: str = "EXECUTED") -> list[dict]:
+def filter_by_state(list_of_dictionaries: list[dict], status: str = "EXECUTED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению."""
-    new_list_dictionary = []
-    for dictionary in list_dictionary:
+    new_list_of_dictionary = []
+    for dictionary in list_of_dictionaries:
         if dictionary["state"] == status:
-            new_list_dictionary.append(dictionary)
-    return new_list_dictionary
+            new_list_of_dictionary.append(dictionary)
+    return new_list_of_dictionary
 
 
-def sort_by_date(list_dictionary: list[dict], is_value: bool = True) -> list[dict]:
+def sort_by_date(list_of_dictionaries: list[dict], is_value: bool = True) -> list[dict]:
     """Функция, которая возвращает список словарей, сортированных по дате"""
-    list_dictionary.sort(key=lambda x: x["date"])
+    list_of_dictionaries.sort(key=lambda x: x["date"])
     if is_value:
-        return list_dictionary[::-1]
+        return list_of_dictionaries[::-1]
     else:
-        return list_dictionary
+        return list_of_dictionaries
