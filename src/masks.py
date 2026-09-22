46b00e5919baa91f9a1526f6ec315ee83a1c4cad
Diana Paramonova <dparamonova638@gmail.com>
2026-09-22 22:34:50 +0300

    add test_processing

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,7 @@
 def get_mask_card_number(number_card: str) -> str:
     """принимает на вход номер карты и возвращает ее маску"""
-    if len(number_card) != 16 or not number_card.isdigit(): return ""
+    if len(number_card) != 16 or not number_card.isdigit():
+        return ""
     answer = number_card[:4] + " "
     answer += number_card[4:6] + "** "
     answer += "**** " + number_card[-4:]
@@ -9,5 +10,6 @@ def get_mask_card_number(number_card: str) -> str:
 
 def get_mask_account(mask: str) -> str:
     """принимает на вход номер счета и возвращает его маску"""
-    if len(mask) != 20 or not mask.isdigit(): return ""
+    if len(mask) != 20 or not mask.isdigit():
+        return ""
     return "**" + mask[-4:]
